6725fea61530bae177b83a57d1b2aba975ff3f18
Melu Franco <melu.franco@gmail.com>
2019-02-05 16:28:48 -0300

    Post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,12 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Post::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
-        //
+        'content' => $faker->paragraph,
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'likes' => $faker->numberBetween(0,300)
     ];
 });
