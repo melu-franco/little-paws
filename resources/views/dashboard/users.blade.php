78193f00d3f34dd18af97f61d85368df8f3524f5
Melu Franco <melu.franco@gmail.com>
2019-02-05 16:27:21 -0300

    Follow / unfollow users

--- resources/views/dashboard/users.blade.php
+++ resources/views/dashboard/users.blade.php
@@ -11,6 +11,27 @@
                     <img src="/uploads/avatars/{{ $user->avatar }}" alt="{{ $user->name }}" style="border-radius:50%;height:3em;object-fit:contain;">
                 </a>
                 <a href="/profile/{{$user->id}}">{{ $user->name }}</a>
+
+                @if(Auth::user()->is_following($user->id))
+                    <form method="post" action="{{ route('user.unfollow', $user->id) }}" >
+                        @csrf
+                        <div class="field">
+                            <div class="control">
+                                <button type="submit" class="button is-light">Unfollow</button>
+                            </div>
+                        </div>
+                    </form>
+                @else
+                    <form method="post" action="{{ route('user.follow', $user->id) }}" >
+                        @csrf
+                        <div class="field">
+                            <div class="control">
+                                <button type="submit" class="button is-light">Follow</button>
+                            </div>
+                        </div>
+                    </form>
+                @endif
+
             </div>
             @endforeach
         </div>
